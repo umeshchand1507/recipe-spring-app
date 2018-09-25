e5477fc5378ac41d9620fc49d22865dd665dceb8
Umesh Chand <umechan@amazon.com>
2018-09-25 22:30:12 +0530

    Maven project added

---
+++ src/main/java/com/bitsfy/recipespringapp/RecipeSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.bitsfy.recipespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeSpringAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bitsfy/recipespringapp/RecipeSpringAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bitsfy.recipespringapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeSpringAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
